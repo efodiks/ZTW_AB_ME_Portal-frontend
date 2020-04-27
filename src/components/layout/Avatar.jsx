cb0a538a6a683ca630d0e37e31581d433afc469e
adobek <45313672+adobek@users.noreply.github.com>
2020-04-27 12:05:05 +0200

    Changed the display of avatar to keep image aspect ratio

--- src/components/layout/Avatar.jsx
+++ src/components/layout/Avatar.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Image } from 'react-bootstrap';
 
 const Avatar = ({ imgSrc, size = "3.5" }) => {
 
     return (
-        <div className="profile-picture" style={{width: size+"em", height: size+"em"}}>
-            {/* [ME] removing attribute 'responsive' because it causes an error: Received `true` for a non-boolean attribute `responsive`. What should it do? */ }
-            <Image src={imgSrc}/>
+        <div className="profile-picture"
+             style={{width: size+"em", height: size+"em", backgroundImage: `url(${imgSrc})`}}>
         </div>
     )
 }
