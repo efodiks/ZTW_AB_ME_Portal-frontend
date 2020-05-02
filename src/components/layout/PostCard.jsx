46a96878a5e1f4f80bd8da1a1ca3ed314ef6d6ec
adobek <45313672+adobek@users.noreply.github.com>
2020-05-02 19:22:16 +0200

    Added link to profile page of post author

--- src/components/layout/PostCard.jsx
+++ src/components/layout/PostCard.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import Avatar from '../layout/Avatar.jsx';
 import { Card } from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 
 const PostCard = ({ author, imgSrc, description }) => {
     
     return (
         <Card style={{padding: "0.5em"}}>
-            <div style={{display: "flex", alignItems: "center", margin: "0em 0.2em 0.4em 0.2em"}}>
-                {author.url && <Avatar
-                    imgSrc={author.url}
-                    size="3.5"
-                />}
-                <Card.Title className="m-2">{author.username}</Card.Title>
-            </div>
+            <Link to={`/user/${author.id}`} className="user-link">
+                <div style={{display: "inline-flex", alignItems: "center", margin: "0em 0.2em 0.5em 0.2em"}}>
+                    {author.url && <Avatar
+                        imgSrc={author.url}
+                        size="3.5"
+                    />}
+                    <Card.Title className="m-3">{author.username}</Card.Title>
+                </div>
+            </Link>
             <Card.Img variant="top" src={imgSrc}/>
             <Card.Body style={{padding: "0.2em"}}>
                 <Card.Text>{description}</Card.Text>
